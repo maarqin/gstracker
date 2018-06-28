671e01d148817887a8605df0a3ce26cc4a603017
Marcos Thomaz <marcos@iMac-de-Dev-Mobile.local>
2018-06-28 18:12:05 -0300

    second commit

--- src/android/GSTracker.java
+++ src/android/GSTracker.java
@@ -1,32 +1,38 @@
-package gstracker;
 
-import org.apache.cordova.CordovaPlugin;
+package com.gstracker.cordova.plugin;
+
+// Cordova-required packages
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+public class ToastyPlugin extends CordovaPlugin {
 
-/**
- * This class echoes a string called from JavaScript.
- */
-public class GSTracker extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
+  @Override
+  public boolean execute(String action, JSONArray args,
+    final CallbackContext callbackContext) {
+      // Verify that the user sent a 'show' action
+      if (!action.equals("show")) {
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
         return false;
-    }
+      }
+      String message;
+      String duration;
+      try {
+        JSONObject options = args.getJSONObject(0);
+        message = options.getString("message");
+        duration = options.getString("duration");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
-    }
+
+      // Send a positive result to the callbackContext
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      callbackContext.sendPluginResult(pluginResult);
+      return true;
+  }
 }
--- www/GSTracker.js
+++ www/GSTracker.js
@@ -1,5 +1,22 @@
-var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'GSTracker', 'coolMethod', [arg0]);
+// Empty constructor
+function GSTracker() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+GSTracker.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'GSTracker', 'show', [options]);
+}
+
+// Installation constructor that binds GSTracker to window
+GSTracker.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.gsTracker = new GSTracker();
+  return window.plugins.toastyPlugin;
 };
+cordova.addConstructor(GSTracker.install);

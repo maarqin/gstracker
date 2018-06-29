0422cd91954c5716486574bc6ac8b38d61541db4
Marcos Thomaz <marcos@iMac-de-Dev-Mobile.local>
2018-06-29 17:15:13 -0300

    create hook file

---
+++ hook/hook.js
@@ -0,0 +1,147 @@
+function keepLine(acc, line) {
+  return `${acc}
+  ${line}`;
+  }
+  
+  function writeImports(acc, line) {
+  return `${acc}
+  ${line}
+  import android.Manifest;
+  import android.content.ComponentName;
+  import android.content.Context;
+  import android.content.Intent;
+  import android.content.IntentFilter;
+  import android.content.ServiceConnection;
+  import android.os.IBinder;
+  import android.support.v4.content.LocalBroadcastManager;
+  import com.gstracker.cordova.plugin.SensorActivityService;`;
+  }
+  
+  function insertOnCreate(acc, line) {
+  return `${acc}
+  ${line}
+          myReceiver = new SensorActivityService.MyReceiver();
+          intent = new Intent(this, SensorActivityService.class);`;
+  }
+  
+  function gsTrackMethods(acc, line) {
+  return `${acc}
+  ${line}
+  
+      private SensorActivityService.MyReceiver myReceiver;
+      public SensorActivityService mSensor = null;
+      private boolean mBound = false;
+      private Intent intent;
+  
+      private final ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+          SensorActivityService.LocalBinder binder = (SensorActivityService.LocalBinder) service;
+          mSensor = binder.getService();
+          mBound = true;
+        }
+  
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+          mSensor = null;
+          mBound = false;
+        }
+      };
+  
+      @Override
+      protected void onStart() {
+        super.onStart();
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+      }
+  
+      @Override
+      protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(myReceiver, new IntentFilter(SensorActivityService.ACTION_BROADCAST));
+      }
+  
+      @Override
+      protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(myReceiver);
+        super.onPause();
+      }
+  
+      @Override
+      protected void onStop() {
+        if (mBound) {
+          unbindService(mServiceConnection);
+          mBound = false;
+        }
+        super.onStop();
+      }
+  
+      public void run() {
+        mSensor.requestActivityUpdates(intent);
+      }
+  
+      public void exit() {
+        mSensor.removeActivityUpdates(intent);
+        stopService(intent);
+      }`;
+  }
+
+module.exports = ctx => {
+  if (ctx.opts.platforms.indexOf('android') < 0)
+    return;
+	const fs = ctx.requireCordovaModule('fs');
+	const path = ctx.requireCordovaModule('path');
+	const deferral = ctx.requireCordovaModule('q').defer();
+
+  const mainActivityPath = path.join(platformRoot, 'src/br/com/golsat/golfleetdriver/MainActivity.java');
+  fs.readFile(mainActivityPath, 'utf-8', (err, data) => {
+    if (err)
+      deferral.resolve();
+
+    const fileLines = data.split('\n');
+    const editedFile = fileLines.reduce((acc, line, index, arr) => {
+      if (line.includes('package'))
+        return writeImports(acc, line);
+      else if (line.includes('loadUrl'))
+        return insertOnCreate(acc, line);
+      else if (index == arr.length - 2)
+        return gsTrackMethods(acc, line);
+      else
+        return keepLine(acc, line);
+    }, '');
+    
+    fs.writeFile(mainActivityPath, editedFile, err => {
+      if(err)
+        deferral.resolve();
+      deferral.resolve();
+    });
+  });
+
+  return deferral.promise;
+};
+
+
+
+// const fs = require('fs');
+
+// fs.readFile('MainActivity.java', 'utf-8', (err, data) => {
+// 	if (err)
+// 		throw err;
+
+// 	const fileLines = data.split('\n');
+// 	const editedFile = fileLines.reduce((acc, line, index, arr) => {
+// 		if (line.includes('package'))
+// 			return writeImports(acc, line);
+// 		else if (line.includes('loadUrl'))
+// 			return insertOnCreate(acc, line);
+// 		else if (index == arr.length - 2)
+// 			return gsTrackMethods(acc, line);
+// 		else
+// 			return keepLine(acc, line);
+//   }, '');
+  
+//   fs.writeFile('MainActivity.java', editedFile, err => {
+//     if(err)
+//       throw err;
+//     console.log('Done!');
+//   });
+// });

e8d6b4f1e81ac625dbed8d5056066ba89621f5f3
Marcos Thomaz <marcos@iMac-de-Dev-Mobile.local>
2018-08-24 17:19:23 -0300

    ios hook part

--- hook/ios/hook.js
+++ hook/ios/hook.js
@@ -1,5 +1,73 @@
-function replaceAppDelegate(acc, line) {
+function replaceAppDelegateM(acc, line) {
   return`
+  //gstrack modified
+  /*
+   Licensed to the Apache Software Foundation (ASF) under one
+   or more contributor license agreements.  See the NOTICE file
+   distributed with this work for additional information
+   regarding copyright ownership.  The ASF licenses this file
+   to you under the Apache License, Version 2.0 (the
+   "License"); you may not use this file except in compliance
+   with the License.  You may obtain a copy of the License at
+   
+   http://www.apache.org/licenses/LICENSE-2.0
+   
+   Unless required by applicable law or agreed to in writing,
+   software distributed under the License is distributed on an
+   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+   KIND, either express or implied.  See the License for the
+   specific language governing permissions and limitations
+   under the License.
+   */
+  
+  #import "AppDelegate.h"
+  #import "MainViewController.h"
+  #import "AppDelegate.h"
+  #import "GLManager.h"
+  
+  @implementation AppDelegate
+  
+  - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
+  {
+      self.viewController = [[MainViewController alloc] init];
+      [GLManager sharedManager];
+      
+      if([launchOptions objectForKey:UIApplicationLaunchOptionsLocationKey]) {
+          [[GLManager sharedManager] logAction:@"application_launched_with_location"];
+      }
+      
+      return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  }
+  
+  
+  - (void)applicationDidEnterBackground:(UIApplication *)application {
+      NSLog(@"Application is entering the background");
+      [[GLManager sharedManager] applicationDidEnterBackground];
+  }
+  
+  - (void)applicationWillTerminate:(UIApplication *)application {
+      NSLog(@"Application is terminating");
+      [[GLManager sharedManager] applicationWillTerminate];
+  }
+  
+  - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+  {
+      if([[url host] isEqualToString:@"setup"]) {
+          NSString *endpoint = [[url query] stringByRemovingPercentEncoding];
+          NSLog(@"Saving new API Endpoint: %@", endpoint);
+          [[NSUserDefaults standardUserDefaults] setObject:endpoint forKey:GLAPIEndpointDefaultsName];
+          [[NSUserDefaults standardUserDefaults] synchronize];
+      }
+      
+      return YES;
+  }
+  
+  @end
+  `;
+}
+
+function replaceAppDelegateH(acc, line) {
+  return `
   //gstrack modified
   /*
   Licensed to the Apache Software Foundation (ASF) under one
@@ -9,9 +77,9 @@ function replaceAppDelegate(acc, line) {
   to you under the Apache License, Version 2.0 (the
   "License"); you may not use this file except in compliance
   with the License.  You may obtain a copy of the License at
- 
+  
   http://www.apache.org/licenses/LICENSE-2.0
- 
+  
   Unless required by applicable law or agreed to in writing,
   software distributed under the License is distributed on an
   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -19,50 +87,248 @@ function replaceAppDelegate(acc, line) {
   specific language governing permissions and limitations
   under the License.
   */
- 
- #import "AppDelegate.h"
- #import "MainViewController.h"
- #import "AppDelegate.h"
- #import "GLManager.h"
- 
- @implementation AppDelegate
- 
- - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
- {
-     self.viewController = [[MainViewController alloc] init];
-     [GLManager sharedManager];
-    
-     if([launchOptions objectForKey:UIApplicationLaunchOptionsLocationKey]) {
-         [[GLManager sharedManager] logAction:@"application_launched_with_location"];
-     }
 
-     return [super application:application didFinishLaunchingWithOptions:launchOptions];
- }
+  //
+  //  AppDelegate.h
+  //  Golfleet Driver
+  //
+  //  Created by ___FULLUSERNAME___ on ___DATE___.
+  //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+  //
 
+  #import <Cordova/CDVViewController.h>
+  #import <Cordova/CDVAppDelegate.h>
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-  NSLog(@"Application is entering the background");
-  [[GLManager sharedManager] applicationDidEnterBackground];
+  @interface AppDelegate : CDVAppDelegate {}
+
+  @end
+  `;
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-  NSLog(@"Application is terminating");
-  [[GLManager sharedManager] applicationWillTerminate];
-}
+function replaceMainCommandDelegateM(acc, line) {
+  return `
+  // gstrack modified
+  /*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+  */
 
-- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
-{
-  if([[url host] isEqualToString:@"setup"]) {
-      NSString *endpoint = [[url query] stringByRemovingPercentEncoding];
-      NSLog(@"Saving new API Endpoint: %@", endpoint);
-      [[NSUserDefaults standardUserDefaults] setObject:endpoint forKey:GLAPIEndpointDefaultsName];
-      [[NSUserDefaults standardUserDefaults] synchronize];
+  //
+  //  MainViewController.h
+  //  Golfleet Driver
+  //
+  //  Created by ___FULLUSERNAME___ on ___DATE___.
+  //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+  //
+
+  #import "MainViewController.h"
+  #import "GSTracker.h"
+  #import "GLManager.h"
+
+
+  @implementation MainViewController
+
+  - (id)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil
+  {
+      self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+      if (self) {
+          // Uncomment to override the CDVCommandDelegateImpl used
+          // _commandDelegate = [[MainCommandDelegate alloc] initWithViewController:self];
+          // Uncomment to override the CDVCommandQueue used
+          // _commandQueue = [[MainCommandQueue alloc] initWithViewController:self];
+      }
+      return self;
+  }
+
+  - (id)init
+  {
+      self = [super init];
+      if (self) {
+          // Uncomment to override the CDVCommandDelegateImpl used
+          // _commandDelegate = [[MainCommandDelegate alloc] initWithViewController:self];
+          // Uncomment to override the CDVCommandQueue used
+          // _commandQueue = [[MainCommandQueue alloc] initWithViewController:self];
+      }
+      return self;
+  }
+
+  - (void)didReceiveMemoryWarning
+  {
+      // Releases the view if it doesn't have a superview.
+      [super didReceiveMemoryWarning];
+      
+      // Release any cached data, images, etc that aren't in use.
+  }
+
+  #pragma mark View lifecycle
+
+  - (void)viewWillAppear:(BOOL)animated
+  {
+      // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
+      // you can do so here.
+      
+      [super viewWillAppear:animated];
+  }
+
+  - (void)viewDidLoad
+  {
+      [super viewDidLoad];
+      // Do any additional setup after loading the view from its nib.
+  }
+
+  - (void)viewDidUnload
+  {
+      [super viewDidUnload];
+      // Release any retained subviews of the main view.
+      // e.g. self.myOutlet = nil;
+  }
+
+  - (void) run:(NSDictionary *) dic {
+      NSString* userId = [dic objectForKey:@"userId"];
+      NSString* userEmail = [dic objectForKey:@"userEmail"];
+      NSString* userDeviceId = [dic objectForKey:@"userDeviceId"];
+      
+      [[NSUserDefaults standardUserDefaults] setValue:userId forKey:USER_ID];
+      [[NSUserDefaults standardUserDefaults] setValue:userEmail forKey:USER_EMAIL];
+      [[NSUserDefaults standardUserDefaults] setValue:userDeviceId forKey:USER_DEVICE_ID];
+      [[NSUserDefaults standardUserDefaults] setValue:AVAILABLE forKey:IS_CONNECTION_OK];
+  }
+
+  - (void) exit {
+      [[NSUserDefaults standardUserDefaults] setValue:NOTHING forKey:IS_CONNECTION_OK];
+      
+      [[GLManager sharedManager] stopAllUpdates];
+      [[GLManager sharedManager] endTrip];
+  }
+
+  /* Comment out the block below to over-ride */
+
+  /*
+  - (UIWebView*) newCordovaViewWithFrame:(CGRect)bounds
+  {
+  return[super newCordovaViewWithFrame:bounds];
   }
   
-  return YES;
+  // CB-12098
+  #if __IPHONE_OS_VERSION_MAX_ALLOWED < 90000
+  - (NSUInteger)supportedInterfaceOrientations
+  #else
+  - (UIInterfaceOrientationMask)supportedInterfaceOrientations
+  #endif
+  {
+  return [super supportedInterfaceOrientations];
+  }
+  
+  - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+  {
+  return [super shouldAutorotateToInterfaceOrientation:interfaceOrientation];
+  }
+  
+  - (BOOL)shouldAutorotate
+  {
+  return [super shouldAutorotate];
+  }
+  */
+
+  @end
+
+  @implementation MainCommandDelegate
+
+  /* To override the methods, uncomment the line in the init function(s)
+  in MainViewController.m
+  */
+
+  #pragma mark CDVCommandDelegate implementation
+
+  - (id)getCommandInstance:(NSString*)className
+  {
+      return [super getCommandInstance:className];
+  }
+
+  - (NSString*)pathForResource:(NSString*)resourcepath
+  {
+      return [super pathForResource:resourcepath];
+  }
+
+  @end
+
+  @implementation MainCommandQueue
+
+  /* To override, uncomment the line in the init function(s)
+  in MainViewController.m
+  */
+  - (BOOL)execute:(CDVInvokedUrlCommand*)command
+  {
+      return [super execute:command];
+  }
+
+  @end
+
+
+  `;
 }
- 
- @end`;
+
+function replaceMainCommandDelegateH(acc, line) {
+  return `
+  // gstrack modified
+  /*
+  Licensed to the Apache Software Foundation (ASF) under one
+  or more contributor license agreements.  See the NOTICE file
+  distributed with this work for additional information
+  regarding copyright ownership.  The ASF licenses this file
+  to you under the Apache License, Version 2.0 (the
+  "License"); you may not use this file except in compliance
+  with the License.  You may obtain a copy of the License at
+  
+  http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on an
+  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  KIND, either express or implied.  See the License for the
+  specific language governing permissions and limitations
+  under the License.
+  */
+
+  //
+  //  MainViewController.h
+  //  Golfleet Driver
+  //
+  //  Created by ___FULLUSERNAME___ on ___DATE___.
+  //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+  //
+
+  #import <Cordova/CDVViewController.h>
+  #import <Cordova/CDVCommandDelegateImpl.h>
+  #import <Cordova/CDVCommandQueue.h>
+
+  @interface MainViewController : CDVViewController
+
+  - (void) run:(NSDictionary *) dic;
+  - (void) exit;
+
+  @end
+
+  @interface MainCommandDelegate : CDVCommandDelegateImpl
+  @end
+
+  @interface MainCommandQueue : CDVCommandQueue
+  @end
+  `;
 }
 
 
